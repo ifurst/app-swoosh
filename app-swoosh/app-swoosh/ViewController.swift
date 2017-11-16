931ec37421449fe10304f170f60029871a9c4961
Vinod Gnaniah <vinod@Vinods-Mac.local>
2017-11-16 04:49:44 -0800

    Init

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Vinod Gnaniah on 11/16/17.
+//  Copyright © 2017 Vinod Gnaniah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
