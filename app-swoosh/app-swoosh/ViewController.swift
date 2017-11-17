4c4b96987ecd82a2b4d47177179ced14881867db
Vinod Gnaniah <vinod@Vinods-Mac.local>
2017-11-17 07:45:16 -0800

    lesson 05

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -16,11 +16,8 @@ class ViewController: UIViewController {
         
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func unwindFromSkillViewController(unwindSegue: UIStoryboardSegue) {
+        
     }
-
-
 }
 
